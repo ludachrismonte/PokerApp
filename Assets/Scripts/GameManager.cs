a79976e66cdce58399e29b3fa05d49ad589b0554
cpmonte <cpmonte@umich.edu>
2018-12-21 18:52:39 -0500

    asdf

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class GameManager : NetworkBehaviour {
+
+    [SyncVar] private int num_registered;
+    [SyncVar] private int current_pot;
+    [SyncVar] private int current_bet;
+
+
+	// Use this for initialization
+	void Start () {
+        num_registered = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    public int Register(GameObject new_player) {
+        num_registered++;
+        return num_registered - 1;
+    }
+
+    public int GetPot() {
+        return current_pot;
+    }
+
+    public void AddToPot(int amt) {
+        current_pot += amt;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour {
+
+    [SyncVar] public int ID = -1;
+    [SyncVar] public int ChipCount;
+    public GameObject Canvas;
+
+    private Text PotText;
+    private Text ChipText;
+
+    private GameManager game_manager;
+
+    // Use this for initialization
+    void Start() {
+        game_manager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+
+        PotText = Canvas.transform.Find("Pot").GetComponent<Text>();
+        ChipText = Canvas.transform.Find("Stack").GetComponent<Text>();
+
+        ID = game_manager.Register(this.gameObject);
+        ChipCount = 500;
+        UpdateChipCount();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        ShowPot();
+    }
+
+    //~~~~~~~~~~~~~BETTING~~~~~~~~~~~~~~//
+    public void bet(int amt) {
+        ChipCount -= amt;
+        UpdateChipCount();
+        CmdBet(amt);
+    }
+
+    [Command]
+    private void CmdBet(int amt) {
+        game_manager.AddToPot(amt);
+    }
+
+    //~~~~~~~~~~~~~GETTERS~~~~~~~~~~~~~~//
+    public int GetChipCount()
+    {
+        return ChipCount;
+    }
+
+    //~~~~~~~~~~~~~UI UPDATES~~~~~~~~~~~~~~//
+    private void ShowPot() {
+        PotText.text = "Pot: " + game_manager.GetPot().ToString();
+    }
+
+    private void UpdateChipCount() {
+        ChipText.text = "Chip Stack: " + ChipCount.ToString();
+    }
+}
